b48bb80267872f19192a86c2387c1006da5d391f
Josep Subils <josep@spellbook.io>
2017-08-02 15:09:50 +0200

    [update] tabs for spaces

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 module.exports = {
-	get : function (url) {
-		url = url.indexOf('://') > -1 ? url.split('/')[2] : url.split('/')[0]
-		url = url.split(':')[0]
-		url = url.split('.')
-		var len = url.length
-		if (len === 2) return url.join('.')
-		else return url[len - 2] + '.' + url[len - 1]
-	},
-	hostname : function (url) {
-		url = url.indexOf('://') > -1 ? url.split('/')[2] : url.split('/')[0]
-		return url.split(':')[0]
-	},
-	origin : function (url) {
-		url = this.hostname(url)
-		return url.replace('www.','')
-	},
-	clean : function (url) {
-		url = url.split('#')[0]
-		return url.slice(-1) === '/'? url.slice(0,-1) : url
-	}
+  get : function (url) {
+    url = url.indexOf('://') > -1 ? url.split('/')[2] : url.split('/')[0]
+    url = url.split(':')[0]
+    url = url.split('.')
+    var len = url.length
+    if (len === 2) return url.join('.')
+    else return url[len - 2] + '.' + url[len - 1]
+  },
+  hostname : function (url) {
+    url = url.indexOf('://') > -1 ? url.split('/')[2] : url.split('/')[0]
+    return url.split(':')[0]
+  },
+  origin : function (url) {
+    url = this.hostname(url)
+    return url.replace('www.','')
+  },
+  clean : function (url) {
+    url = url.split('#')[0]
+    return url.slice(-1) === '/'? url.slice(0,-1) : url
+  }
 }
